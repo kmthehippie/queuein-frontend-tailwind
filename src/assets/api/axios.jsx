f34fb4118ac4930859a0e621c520c1c23bb00a04
kmthehippie <kmthehippie@gmail.com>
2025-11-05 14:17:32 +0800

    Axios VITE_BASE_URL added

--- src/assets/api/axios.jsx
+++ src/assets/api/axios.jsx
@@ -3,19 +3,19 @@ import axios from "axios";
 // const userAgentString = navigator.userAgent;
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
 });
 
 export default api;
 export const apiPrivate = axios.create({
-  baseURL: "/api",
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
   credentials: "include",
 });
 
 export const interceptedApiPrivate = axios.create({
-  baseURL: "/api",
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api`,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
   credentials: "include",
